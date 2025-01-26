9accf9517e5db9b792fac0f167bcdb967fbacc70
Lesia_Nazaruk <nazarullesia@gmail.com>
2025-01-26 21:22:32 +0200

    add local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ContactForm from "./components/contactForm/ContactForm";
 import ContactList from "./components/contactList/ContactList";
 import SearchBox from "./components/searchBox/SearchBox";
@@ -19,12 +19,19 @@ const fieldValidation = Yup.object().shape({
 });
 
 const App = () => {
-  const [contacts, setContacts] = useState([
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem("contacts");
+    return savedContacts ? JSON.parse(savedContacts) : "";
+  }, [
     { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
     { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
   ]);
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
 
   const [searchQuery, setSearchQuery] = useState("");
   const handleSearch = (e) => {
