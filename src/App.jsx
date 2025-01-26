288852dac9aa5fb8a9bffb1126914069f98e9d12
Lesia_Nazaruk <nazarullesia@gmail.com>
2025-01-26 23:07:45 +0200

    add corect

--- src/App.jsx
+++ src/App.jsx
@@ -20,15 +20,17 @@ const fieldValidation = Yup.object().shape({
 });
 
 const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    const savedContacts = localStorage.getItem("contacts");
-    return savedContacts ? JSON.parse(savedContacts) : "";
-  }, [
+  const contactData = [
     { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
     { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
+  ];
+
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem("contacts");
+    return savedContacts ? JSON.parse(savedContacts) : contactData;
+  });
 
   useEffect(() => {
     localStorage.setItem("contacts", JSON.stringify(contacts));
