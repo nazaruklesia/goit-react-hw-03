92b461ea628fe15e7fbf6fbc8d756a5450e5cbac
Lesia_Nazaruk <nazarullesia@gmail.com>
2025-01-26 14:10:35 +0200

    add murkup

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
+// import { Formik } from "formik";
+// import { useState } from "react";
 
+const App = () => {
+  //   const [contacts, setContacts] = useState([
+  //     { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  //     { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  //     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  //     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+  //   ]);
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      {/* <ContactForm />
+      <SearchBox />
+      <ContactList /> */}
+    </div>
+  );
+};
+
+export default App;
